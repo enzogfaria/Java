ccbc7d2868e825a49dc5e38c33754ed82d03d27a
Enzo Faria <123766918+enzogfaria@users.noreply.github.com>
2024-01-03 23:55:03 -0300

    correção

--- workspace/untitled/src/Main.java
+++ workspace/untitled/src/Main.java
@@ -1,12 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         String x;
         x = sc.next();
-        system.out.println("Voce digitou: " + x);
+        System.out.println("Voce digitou: " + x);
         sc.close();
     }
 

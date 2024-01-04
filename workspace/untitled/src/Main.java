b2549afbc91deccf653c6ee6f3246c2e51f1b02d
Enzo Faria <123766918+enzogfaria@users.noreply.github.com>
2024-01-03 23:57:05 -0300

    Update Main.java

--- workspace/untitled/src/Main.java
+++ workspace/untitled/src/Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        String x;
-        x = sc.next();
+        int x;
+        x = sc.nextInt();
         System.out.println("Voce digitou: " + x);
         sc.close();
     }

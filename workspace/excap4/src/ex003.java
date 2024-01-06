444079693dd65d5c101ea439511293b8ee959023
Enzo Faria <123766918+enzogfaria@users.noreply.github.com>
2024-01-06 00:04:33 -0300

    ex004 resolvido

--- workspace/excap4/src/ex003.java
+++ workspace/excap4/src/ex003.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ex003 {
     public static void main (String[] args) {
-        
+    System.out.println("Insira 4 numeros inteiros:");
+
+        Scanner sc = new Scanner(System.in);
+
+        int a, b , c , d, dif;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+        dif = a*b - c*d;
+
+        sc.close();
+
+        System.out.printf("A diferença é: %d", dif);
     }
 }

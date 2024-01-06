a0298f6657b3e48d1ee3aed9e9a72af6273b8ca5
Enzo Faria <123766918+enzogfaria@users.noreply.github.com>
2024-01-05 23:57:54 -0300

    ex002 resolvido

--- workspace/excap4/src/ex002.java
+++ workspace/excap4/src/ex002.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class ex002 {
     public static void main (String[] args) {
-        
+        System.out.println("Insira o raio do circulo:");
 
+        Scanner sc = new Scanner(System.in);
+
+        double raio, area, pi;
+        raio = sc.nextDouble();
+        raio = Math.pow(raio, 2);
+        pi = 3.14159;
+        area = pi*raio;
+
+        sc.close();
+
+        System.out.printf("A area do circulo é: %.4f", area);
     }
 }

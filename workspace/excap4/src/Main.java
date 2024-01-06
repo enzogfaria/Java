3cafe75c0656e930d76fea23a616c48045a0f7df
Enzo Faria <123766918+enzogfaria@users.noreply.github.com>
2024-01-05 23:48:19 -0300

    ex001 resolvido

--- workspace/excap4/src/Main.java
+++ workspace/excap4/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main (String[] args) {
+        System.out.println("Insira 2 números para somar:");
+        Scanner sc = new Scanner(System.in);
+
+        double x, y, res;
+        x = sc.nextDouble();
+        y = sc.nextDouble();
+        res = x + y;
+
+        sc.close();
+
+        System.out.printf("O resultado é %.2f!", res);
     }
 }

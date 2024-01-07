556e948b3321e577088177da4e22ebf5c2075ee8
Enzo Faria <123766918+enzogfaria@users.noreply.github.com>
2024-01-06 23:59:28 -0300

    Update ex003.java

--- workspace/excap4/src/ex003.java
+++ workspace/excap4/src/ex003.java
@@ -8,7 +8,7 @@ public class ex003 {
 
         int a, b , c , d, dif;
 
-        a = sc.nextInt()
+        a = sc.nextInt();f
         b = sc.nextInt();
         c = sc.nextInt();
         d = sc.nextInt();

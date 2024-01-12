e26771e3ed477c820bbb7d1bdf31088bdb68d931
Enzo Faria <123766918+enzogfaria@users.noreply.github.com>
2024-01-12 13:39:14 -0300

    Update ex004.java

--- workspace/excap4/src/ex004.java
+++ workspace/excap4/src/ex004.java
@@ -6,13 +6,16 @@ public class ex004 {
         int nf;
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Insira o número do funciónario:");
         nf = sc.nextInt();
+        System.out.println("Insira o número de horas trabalhadas:");
         ht = sc.nextFloat();
+        System.out.println("Insira o salário por hora:");
         sh = sc.nextFloat();
 
         salario = ht*sh;
         sc.close();
 
-        System.out.printf("O salário do funcionário %d é de: %.2f",nf, salario);
+        System.out.printf("O salário do funcionário %d é de: U$%.2f",nf, salario);
     }
 }
